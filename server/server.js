db564404f5c6117e8f8dae43f0d3b0da0c2894b4
Devin Mui <devinmui@yahoo.com>
2016-08-27 18:07:26 -0700

    server is done, android app is pretty much done, need to test livestream, add map to html

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ var express = require("express")
 var bodyParser = require("body-parser")
 var app = express()
 
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
 var lat, long;
+var panic = false;
 
 app.get('/', function(req, res){
     res.send("ayy lmao")    
@@ -11,7 +15,20 @@ app.get('/', function(req, res){
 app.post('/gps', function(req, res){
 	lat = req.body.lat
 	long = req.body.long
+	panic = true
 	res.send({lat: lat, long: long})
+})
+
+app.get('/gps', function(req, res){
+	if(panic)
+		res.send({ lat: lat, long: long })
+	else
+		res.send({})
+})
+
+app.get('/nopanic', function(req, res){
+	panic = false
+	res.send({})
 })
 
 app.listen(3000, function(){
